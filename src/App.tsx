731ec8eb999e853e5e897f2aae8470be0a36993e
David Martinelli <david.martinelli@farfetch.com>
2022-10-06 19:14:13 +0100

    Use container

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,7 @@
-import {
-  Check,
-  ClipboardText,
-  PlusCircle,
-  Trash,
-  TrashSimple,
-} from 'phosphor-react';
-import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
-import {
-  ChangeEvent,
-  FormEvent,
-  InputHTMLAttributes,
-  InvalidEvent,
-  useState,
-} from 'react';
+import { PlusCircle } from 'phosphor-react';
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react';
 import logotipo from './assets/logo.svg';
-import * as cx from 'classnames';
 import TaskList from './components/TaskList';
-
 export interface Task {
   id: number;
   text: string;
@@ -61,67 +46,63 @@ function App() {
   }
 
   return (
-    <>
-      <div className='bg-gray-600 h-screen w-screen'>
-        <div className='bg-gray-700 w-screen flex flex-col pt-14 pb-20'>
-          <img src={logotipo} alt='Logotipo' className='mx-auto' />
-        </div>
+    <div className='bg-gray-600 h-screen w-screen'>
+      <div className='bg-gray-700 w-screen flex flex-col pt-14 pb-20'>
+        <img src={logotipo} alt='Logotipo' className='mx-auto' />
+      </div>
 
-        <div className='grid grid-cols-12'>
-          <div className='h-14 col-span-2'></div>
-          <div className='w-full h-14 col-span-8'>
-            <form onSubmit={handleSubmit}>
-              <section
-                id='form'
-                className='flex flex-row gap-2 justify-center -mt-7'>
-                <input
-                  name='task'
-                  value={newTask}
-                  autoComplete='off'
-                  onChange={handleNewTaskChange}
-                  onInvalid={handleNewTaskInvalid}
-                  className='bg-gray-400 text-gray-200 placeholder:text-gray-300 rounded-md border border-gray-700 pl-4 h-14 w-full active:outline focus-visible:outline outline-blue outline-2'
-                  type='text'
-                  placeholder='Create a new task'
-                  required
-                />
-
-                <button
-                  type='submit'
-                  className='w-30 py-2 px-4 flex justify-center items-center gap-2 rounded-md bg-blue hover:bg-blue-dark text-gray-100 active:outline focus-visible:outline outline-blue outline-2 transition ease-in duration-200 text-center text-base font-semibold'>
-                  <span>Create</span>
-                  <PlusCircle size={23} />
-                </button>
-              </section>
-            </form>
-
-            <section id='tasks' className='mt-10'>
-              <div className='flex flex-row justify-between mb-10'>
-                <div className='text-blue font-bold'>
-                  New tasks
-                  <span className='ml-2 py-1 px-2 rounded-md bg-gray-400 text-gray-100'>
-                    {tasks.length}
-                  </span>
-                </div>
-                <div className='text-purple font-bold'>
-                  Finished
-                  <span className='ml-2 py-1 px-2 rounded-md bg-gray-400 text-gray-100'>
-                    {tasks.filter((x) => x.completed).length}
-                  </span>
-                </div>
-              </div>
-
-              <TaskList
-                tasks={tasks}
-                onCompleteTask={handleCompleteTask}
-                onDeleteTask={handleDeteleTask}
+      <div className='container mx-auto px-48'>
+        <div className='w-full h-14 col-span-8'>
+          <form onSubmit={handleSubmit}>
+            <section
+              id='form'
+              className='flex flex-row gap-2 justify-center -mt-7'>
+              <input
+                name='task'
+                value={newTask}
+                autoComplete='off'
+                onChange={handleNewTaskChange}
+                onInvalid={handleNewTaskInvalid}
+                className='bg-gray-400 text-gray-200 placeholder:text-gray-300 rounded-md border border-gray-700 pl-4 h-14 w-full active:outline focus-visible:outline outline-blue outline-2'
+                type='text'
+                placeholder='Create a new task'
+                required
               />
+
+              <button
+                type='submit'
+                className='w-30 py-2 px-4 flex justify-center items-center gap-2 rounded-md bg-blue hover:bg-blue-dark text-gray-100 active:outline focus-visible:outline outline-blue outline-2 transition ease-in duration-200 text-center text-base font-semibold'>
+                <span>Create</span>
+                <PlusCircle size={23} />
+              </button>
             </section>
-          </div>
-          <div className='h-14 col-span-2'></div>
+          </form>
+
+          <section id='tasks' className='mt-10'>
+            <div className='flex flex-row justify-between mb-10'>
+              <div className='text-blue font-bold'>
+                New tasks
+                <span className='ml-2 py-1 px-2 rounded-md bg-gray-400 text-gray-100'>
+                  {tasks.length}
+                </span>
+              </div>
+              <div className='text-purple font-bold'>
+                Finished
+                <span className='ml-2 py-1 px-2 rounded-md bg-gray-400 text-gray-100'>
+                  {tasks.filter((x) => x.completed).length}
+                </span>
+              </div>
+            </div>
+
+            <TaskList
+              tasks={tasks}
+              onCompleteTask={handleCompleteTask}
+              onDeleteTask={handleDeteleTask}
+            />
+          </section>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
